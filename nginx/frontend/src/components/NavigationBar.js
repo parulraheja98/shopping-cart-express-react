70b82c6bff09b6f606efd12a46260013b77015e4
parulraheja98 <parulraheja98@gmail.com>
2020-01-09 00:48:10 -0600

    Frontend code refactored

--- nginx/frontend/src/components/NavigationBar.js
+++ nginx/frontend/src/components/NavigationBar.js
@@ -1,30 +1,30 @@
-import React,{Component} from 'react';
-import {Nav,Navbar} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import '../App.css';
 
 class NavigationBar extends Component {
 
-constructor(props) {
-    super(props);
-    this.state = {};
-}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-render() {
+    render() {
 
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Nav className="mr-auto">
-            <Nav.Link href="/">Product Page </Nav.Link>
-            <Nav.Link href="/cart">Cart Page </Nav.Link>
-            <Nav.Link href="/checkout">Checkout</Nav.Link>
-            <Nav.Link href='/createproduct'> Create Product </Nav.Link>
-            </Nav>
-        </Navbar>
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Product Page </Nav.Link>
+                    <Nav.Link href="/cart">Cart Page </Nav.Link>
+                    <Nav.Link href="/checkout">Checkout</Nav.Link>
+                    <Nav.Link href='/createproduct'> Create Product </Nav.Link>
+                </Nav>
+            </Navbar>
 
-    )
+        )
 
 
-}
+    }
 
 }
 
